2ed545c993f9c0ef8c9f186423f38d11eee6dfa2
Andy Oakley <Andy.Oakley@wilko.com>
2017-12-01 15:50:29 +0000

    hmm

--- Scripts/(main) web-data-quality.R
+++ Scripts/(main) web-data-quality.R
@@ -1,9 +1,14 @@
-# Main Program v1.0
-# See ReadMe for details
+# Main Program v1.1
+# December 2012:  Andy Oakley
+# See ReadMe for full details
+# https://github.com/DasOakster/data-quality-management/blob/master/README.md
+# This program creates a single product view across Google and SAP
+# It evaluates the Data Quality for required but missing atributes, completed web descriptions and 
+# consistency between the brand attribute and the product description
 
 #------------------------------------------------------------------------------------------------------
 
-# Set up libraries, working directory and source files
+# Set up libraries, working directory and source scripts containing functions
 
       library(dplyr)
 
@@ -22,24 +27,24 @@
       setwd("..")
       
 # Import the Google Feed, SAP extract, PSA Lookup table and Web Attribute table
-# Source:  create-web-product-data.R
 
+      # Source:  create-web-product-data.R
       import.web.data()
 
 # Tidy up the files so they can be matched
-# Source:  create-web-product-data.R
-      
+
+      # Source:  create-web-product-data.R
       tidy.web.data()
 
 # Merge files together into web_product_file     
-# Source:  create-web-product-data.R
-            
+
+      # Source:  create-web-product-data.R
       merge.web.data()
 
 # Check attributes for completeness
 # Default attribute values to NA if they are not required
-# Source:  tidy-attributes 
-      
+
+      # Source:  tidy-attributes 
       attribute <- unique(web.attribute$Attribute)
       
       for (i in 1:length(attribute)) {
@@ -49,19 +54,23 @@
       }
 
 # Score products with data quality scores
-# Source:  score-product-data-quality
-      
+
+      # Source:  score-product-data-quality
       score.data.quality()
  
 # Summarise Performance
-      
+
+      #Source:  build-output-files, build-psa-files
       dq.summarise.results()
       
+
 # Export Web Data Files
-      
+
+      # export-output-files
       export.product.files()
 
 # Tidy up the environment
       
       score.files <- ls(pattern = ".score")
-      rm(list=ls()[! ls() %in% c("web.product.data","report.data","gtin.issue",score.files)])     
+      rm(list=ls()[! ls() %in% c("web.product.data","report.data","gtin.issue",score.files)]) 
+      message("Process Completed")
--- Scripts/export-output-files.R
+++ Scripts/export-output-files.R
@@ -1,11 +1,11 @@
+# This function exports the data quality management reports, data files for cleansing and report files
       
 export.product.files <- function() {
 
-# Exports th data quality management reports, data for cleansing and report files
-      
+      # Move to the Output Files Directory
       setwd("..")
       setwd("./Output Files")
-      #setwd("R:/Data Quality Reports/Web Data Quality Reports")
+      
 # Output the lists of unmatched web skus and web skus with missing web descriptions
       
       message("Outputting Web Product File")
@@ -44,7 +44,7 @@ export.product.files <- function() {
 
 # Create data for cleansing
             
-# Output the Web Data Quality Lists for Cleansing
+      # Output the Web Data Quality Lists for Cleansing
       
       message("Outputting Web Category Lists")
       unlink("Web Category",recursive = TRUE)
@@ -53,7 +53,7 @@ export.product.files <- function() {
       output.web.category.cleanse()
       setwd("..")
 
-# Output the Missing Web Descriptions at PSA 1 Level
+      # Output the Missing Web Descriptions at PSA 1 Level
       
       message("Outputting Missing Web Descriptions")
       unlink("Missing Web Description",recursive = TRUE)
@@ -62,17 +62,26 @@ export.product.files <- function() {
       output.web.description.cleanse()
       setwd("..")
       
-# Output the PSA Level Files
+      # Output the PSA 2 Level Files
       
-      message("Outputting PSA Lists")
-      unlink("PSA",recursive = TRUE)
-      dir.create("PSA",showWarnings = FALSE)
-      setwd("./PSA")
-      output.psa.cleanse()
+      message("Outputting PSA2 Lists")
+      unlink("PSA 2",recursive = TRUE)
+      dir.create("PSA 2",showWarnings = FALSE)
+      setwd("./PSA 2")
+      output.psa2.cleanse()
+      setwd("..")
+      
+      # Output the PSA 1 Level Files
+      
+      message("Outputting PSA1 Lists")
+      unlink("PSA 1",recursive = TRUE)
+      dir.create("PSA 1",showWarnings = FALSE)
+      setwd("./PSA 1")
+      output.psa1.cleanse()
       setwd("..")
 #---------------------------------------------------------------------------------------------------------------------------------------
       
-# Output the Report Data File
+      # Output the Report Data File
       
       message("Outputting Report Data")
       setwd("./Report Data")
@@ -83,4 +92,27 @@ export.product.files <- function() {
       setwd("./Attribute Completion")
       build.psa1.attribute.summary()
       build.psa2.attribute.summary()
+      
+# Output PSA 1 and PSA 2 files to R Drive folder
+      
+      setwd("R:/Data Quality Reports/Web Data Quality Reports")
+      
+      # PSA 1 Level Files
+      
+      message("Outputting PSA1 Lists to R Drive")
+      unlink("PSA 1",recursive = TRUE)
+      dir.create("PSA 1",showWarnings = FALSE)
+      setwd("./PSA 1")
+      output.psa1.cleanse.test()
+      setwd("..")   
+      
+      # PSA 2 Level Files
+      
+      message("Outputting PSA2 Lists to R Drive")
+      unlink("PSA 2",recursive = TRUE)
+      dir.create("PSA 2",showWarnings = FALSE)
+      setwd("./PSA 2")
+      output.psa.cleanse()
+      setwd("..")
+      
 }
